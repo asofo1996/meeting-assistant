091b120bbb98809d5852a8628b9b7983648deab3
asofo417 <asofo417@naver.com>
2025-07-14 15:09:52 +0900

    Remove eventlet to resolve worker conflict

--- run.py
+++ run.py
@@ -1,13 +1,6 @@
-# run.py (최종 수정본)
-
-import eventlet
-# 다른 모든 것을 임포트하기 전에 monkey_patch를 최상단에서 실행해야 합니다.
-eventlet.monkey_patch()
-
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    # 이 부분은 로컬에서 python run.py로 실행할 때만 사용됩니다.
     socketio.run(app, debug=True, host='127.0.0.1', port=5000)
